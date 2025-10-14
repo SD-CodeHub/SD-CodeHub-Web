536b493913623f8b131448da1cf408b3cc487ea0
SD-CodeHub <codehubsd@gmail.com>
2025-10-14 21:59:13 +0530

    Final update to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://sd-codehub.netlify.app', // 👈 Your live site URL
+      routes: [
+        '/',           // Home
+        '/about',      // About Page
+        '/services',   // Services Page
+        '/portfolio',  // Portfolio Page
+      ],
+      lastmodDateOnly: false, // adds full datetime
+      changefreq: 'weekly',   // SEO hint
+      priority: 0.8           // Default priority for all routes
+    })
+  ],
 })
